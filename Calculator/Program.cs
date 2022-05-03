e69944cb7ab1efe676450f1c671c367ed9141604
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-03 18:37:05 +0530

    calculator base

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Calculator
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Calculate cal = new Calculate();
+            Console.WriteLine("Enter 1st number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
